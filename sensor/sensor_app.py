80a8910eee3d7ebf4d875bd21656bb5af5f916f5
airbbk <airack01@dcs.bbk.ac.uk>
2022-04-03 19:54:03 +0100

    module3

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,7 @@
 # Runner script for all modules
 from load_data import load_sensor_data
 from house_info import HouseInfo
+from temperature_info import TemperatureData
 
 from datetime import date, datetime
 
@@ -26,6 +27,18 @@ recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("\House sensor records for date: {} = {}".format(test_date.strftime(r"%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
+temperature_data = TemperatureData(data)
+recs = temperature_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Temperature sensor records for date: {} = {}".format(
+    test_date.strftime(r"%m/%d/%y", len(recs))
+))
+
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
 
 # Module 4 code here:
 
